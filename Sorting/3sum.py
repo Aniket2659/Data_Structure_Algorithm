aa31aea29101850d094293def948c1d7b37cd047
Aniket2659 <aniket2659@gmail.com>
2024-07-29 14:56:07 +0530

    [aniket] add: program to implement 3sum problem

---
+++ Sorting/3sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = set()
+        
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        result.add((nums[i], nums[j], nums[k]))
+        
+        return [list(triplet) for triplet in result]
+
+        
